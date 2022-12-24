a04cd89a2e594e89dcd72240ec603fe6af27172e
Jorge Jimenez <jjimenezdiaz@quental.com>
2022-12-24 20:52:37 +0100

    add form design

--- components/ui/NewEntry.tsx
+++ components/ui/NewEntry.tsx
@@ -1,43 +1,61 @@
-import { AddCircleOutlined, SaveOutlined } from '@mui/icons-material'
+import React, { useState } from 'react'
 import { Button, Box, TextField } from '@mui/material'
-import React from 'react'
+import { AddCircleOutlined, SaveOutlined } from '@mui/icons-material'
 
 const NewEntry: React.FC = () => {
+  const [isAdding, setIsAdding] = useState(false)
+  const [inputValue, setInputValue] = useState('')
+  const [isTouched, setIsTouched] = useState(false)
+
+  const handleSave = () => {
+    if (inputValue.length === 0) return 
+    console.log(inputValue)
+  }
+
   return <>
 
    <Box sx={{ marginBottom: 2, paddingX: 2 }}>
-        <Button
-            startIcon={<AddCircleOutlined />}
-            fullWidth
-            variant='outlined'
-        >
-            Agregar tarea
-        </Button>
-        
-        <TextField 
-            fullWidth
-            sx={{ marginTop: 2, marginBottom: 1 }}
-            placeholder={'Write here...'}
-            multiline
-            autoFocus
-            label='New Entry'
-            helperText='Insert a value'
-        />
+       {isAdding ? <>
+            <TextField 
+                fullWidth
+                sx={{ marginTop: 2, marginBottom: 1 }}
+                placeholder={'Write here...'}
+                multiline
+                onBlur={() => setIsTouched(true)}
+                autoFocus
+                error={inputValue.length <= 0 && isTouched}
+                value={inputValue}
+                onChange={(e) => setInputValue(e.target.value)}
+                label='New Entry'
+                helperText='Insert a value'
+            />
 
-        <Box display={'flex'} justifyContent='space-between'>
+            <Box display={'flex'} justifyContent='space-between'>
+                <Button
+                    variant='text'
+                    onClick={() => setIsAdding(false)}
+                >
+                    Cancelar
+                </Button>
+                <Button
+                    variant='outlined'
+                    color='secondary'
+                    endIcon={<SaveOutlined />}
+                    onClick={handleSave}
+                >
+                    Guardar
+                </Button>
+            </Box>
+       </> : <>
             <Button
-                variant='text'
-            >
-                Cancelar
-            </Button>
-            <Button
+                startIcon={<AddCircleOutlined />}
+                fullWidth
                 variant='outlined'
-                color='secondary'
-                endIcon={<SaveOutlined />}
+                onClick={() => setIsAdding(true)}
             >
-                Guardar
+                Agregar tarea
             </Button>
-        </Box>
+       </>}
    </Box>
   </>
 }
